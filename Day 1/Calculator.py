b5513113bdd3aff2af690e421e81662350bbf71b
raga-aravind <ragaaaravind@gmail.com>
2025-04-10 11:02:54 +0530

    Python str,int,variable,len,print,input,stringf exercise basic

---
+++ Day 1/Calculator.py
@@ -0,0 +1,17 @@
+# Welcome to the "Simple Calculator" exercise!
+
+#create variable
+num1 = int(input("Enter The number "))
+num2 = int(input("Enter the number "))
+
+#Basic operations
+sum = num1 + num2
+diff = num1 - num2
+mul = num1 * num2
+
+#Display Result
+print(f"Sum Result: {sum}")
+print(f"Diff Result: {diff}")
+print(f"Multiple Result: {mul}")
+
+print("Thank you")
---
+++ Day 1/Excersie 1.py
@@ -0,0 +1,10 @@
+# simple program using string, int and variable
+
+# define string value
+name = input("Enter Your Name : ")
+
+# define integer value
+age = int(input("Enter Your age : "))
+
+# combine string and integer values
+print(f"Hello, {name} your are {age} years old")
